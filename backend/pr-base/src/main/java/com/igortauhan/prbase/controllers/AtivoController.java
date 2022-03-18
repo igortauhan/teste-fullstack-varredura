dd2bfa3cde5fc93af19d70ae772744f0ac70fc0b
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-03-18 12:21:34 -0300

    Implementado metodo findAll do Ativo

--- backend/pr-base/src/main/java/com/igortauhan/prbase/controllers/AtivoController.java
+++ backend/pr-base/src/main/java/com/igortauhan/prbase/controllers/AtivoController.java
@@ -1,22 +1,27 @@
 package com.igortauhan.prbase.controllers;
 
+import com.igortauhan.prbase.models.Ativo;
 import com.igortauhan.prbase.services.AtivoService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/ativos")
 public class AtivoController {
 
-    private AtivoService ativoService;
+    private final AtivoService ativoService;
 
     public AtivoController(AtivoService ativoService) {
         this.ativoService = ativoService;
     }
 
     @GetMapping()
-    public String helloWorld() {
-        return "Hello, world!";
+    public ResponseEntity<List<Ativo>> findAll() {
+        List<Ativo> ativos = ativoService.findAll();
+        return ResponseEntity.ok(ativos);
     }
 }
--- backend/pr-base/src/main/java/com/igortauhan/prbase/services/AtivoService.java
+++ backend/pr-base/src/main/java/com/igortauhan/prbase/services/AtivoService.java
@@ -1,14 +1,21 @@
 package com.igortauhan.prbase.services;
 
+import com.igortauhan.prbase.models.Ativo;
 import com.igortauhan.prbase.repositories.AtivoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AtivoService {
 
-    private AtivoRepository ativoRepository;
+    private final AtivoRepository ativoRepository;
 
     public AtivoService(AtivoRepository ativoRepository) {
         this.ativoRepository = ativoRepository;
+    }
+
+    public List<Ativo> findAll() {
+         return ativoRepository.findAll();
     }
 }
