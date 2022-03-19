f104f62f329fbfc29b5e65f5e46f7e9b4c5772c6
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-03-19 12:26:45 -0300

    Implementado rotina de busca por Dominio ou Email no projeto pr-search. Chamando endpoint de insercao de Records no projeto pr-base

--- backend/pr-search/src/main/java/com/igortauhan/prsearch/services/SearchService.java
+++ backend/pr-search/src/main/java/com/igortauhan/prsearch/services/SearchService.java
@@ -1,20 +1,34 @@
 package com.igortauhan.prsearch.services;
 
+import com.igortauhan.prsearch.config.GsonConfig;
+import com.igortauhan.prsearch.feign.RecordsFeignClient;
 import com.igortauhan.prsearch.models.GenericRequestClass;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.*;
+import com.igortauhan.prsearch.models.Records;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SearchService {
 
     private final RequestService requestService;
+    private final GsonConfig gsonConfig;
+    private final RecordsFeignClient recordsFeignClient;
 
-    public SearchService(RequestService requestService, RestTemplateBuilder restTemplateBuilder) {
+    public SearchService(RequestService requestService, GsonConfig gsonConfig, RecordsFeignClient recordsFeignClient) {
         this.requestService = requestService;
+        this.gsonConfig = gsonConfig;
+        this.recordsFeignClient = recordsFeignClient;
     }
 
-    public GenericRequestClass search(String pattern) {
-        return requestService.search(pattern);
+    public GenericRequestClass search(String pattern, Long ativoId) {
+        GenericRequestClass genericRequestClass = requestService.search(pattern);
+        List<Records> records = insertRecord(genericRequestClass, ativoId);
+        return genericRequestClass;
+    }
+
+    private List<Records> insertRecord(GenericRequestClass genericRequestClass, Long ativoId) {
+        String recordsJson = gsonConfig.gson().toJson(genericRequestClass, GenericRequestClass.class);
+        return recordsFeignClient.insert(recordsJson, ativoId).getBody();
     }
 }
