dcff662ff77988682714e905fcd5894259f0a400
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-03-19 14:53:40 -0300

    Adicionado projeto pr-gateway para roteamento de rotas com Spring Cloud Gateway

---
+++ backend/pr-gateway/src/main/java/com/igortauhan/prgateway/PrGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.igortauhan.prgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrGatewayApplication.class, args);
+	}
+
+}
