bbc0538e662af60cab365d675034ac58f703a5bd
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-03-19 14:29:24 -0300

    Adicionado projeto pr-eureka-server para registrar um servidor Eureka para a aplicacao

---
+++ backend/pr-eureka-server/src/test/java/com/igortauhan/preurekaserver/PrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.igortauhan.preurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
