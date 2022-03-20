044cb94d03dd4505b5d09b67e1057d8407464b65
Igor Tauhan <igortauhan.ds31@gmail.com>
2022-03-20 15:39:55 -0300

    Adicionado LOGS das rotinas de monitoramento

--- backend/pr-base/src/main/java/com/igortauhan/prbase/controllers/RecordsController.java
+++ backend/pr-base/src/main/java/com/igortauhan/prbase/controllers/RecordsController.java
@@ -2,6 +2,8 @@ package com.igortauhan.prbase.controllers;
 
 import com.igortauhan.prbase.models.Records;
 import com.igortauhan.prbase.services.RecordsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @RequestMapping(value = "/records")
 public class RecordsController {
 
+    private static Logger logger = LoggerFactory.getLogger(RecordsController.class);
     private final RecordsService recordsService;
 
     public RecordsController(RecordsService recordsService) {
@@ -19,6 +22,8 @@ public class RecordsController {
 
     @PostMapping
     public ResponseEntity<List<Records>> insert(@RequestBody String recordsJson, @RequestParam Long ativoId) {
+        logger.info("Requisicao recebida no endpoint /records");
+        logger.info("ID do Ativo a ser registrado na consulta: {}", ativoId);
         List<Records> records = recordsService.insert(recordsJson, ativoId);
         return ResponseEntity.ok(records);
     }
--- backend/pr-search/src/main/java/com/igortauhan/prsearch/services/RequestService.java
+++ backend/pr-search/src/main/java/com/igortauhan/prsearch/services/RequestService.java
@@ -4,6 +4,8 @@ import com.igortauhan.prsearch.models.GenericRequestClass;
 import com.igortauhan.prsearch.models.intx.RequestBody;
 import com.igortauhan.prsearch.models.intx.ResponseBodyRequest;
 import com.igortauhan.prsearch.services.utils.PrepareRequestHeaders;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @Service
 public class RequestService {
 
+    Logger logger = LoggerFactory.getLogger(RequestService.class);
     private final RestTemplate restTemplate;
     private final PrepareRequestHeaders prepareRequestHeaders;
 
@@ -23,11 +26,13 @@ public class RequestService {
     public GenericRequestClass search(String pattern) {
         // Pega o resultado do request na url de POST
         ResponseBodyRequest bodyRequest = sendPost(pattern);
+        logger.debug("ID: {}", bodyRequest.getId());
         // Adiciona o ID no final da url de GET
         String getUrl = "https://2.intelx.io/intelligent/search/result?id=";
         String url = getUrl + bodyRequest.getId();
 
         HttpEntity<String> entity = new HttpEntity<>("parameters", prepareRequestHeaders.getReadyHeaders());
+        logger.info("Fazendo requisicao GET na API da INTX endpoint /result com ID: {}", bodyRequest.getId());
         return restTemplate.exchange(url, HttpMethod.GET, entity, GenericRequestClass.class).getBody();
     }
 
@@ -36,6 +41,7 @@ public class RequestService {
 
         HttpEntity<RequestBody> entity = new HttpEntity<>(requestBody, prepareRequestHeaders.getReadyHeaders());
         String postUrl = "https://2.intelx.io/intelligent/search";
+        logger.info("Fazendo requisicao POST na API da INTX com o padrao: {}", pattern);
         return restTemplate.exchange(postUrl, HttpMethod.POST, entity, ResponseBodyRequest.class).getBody();
     }
 }
